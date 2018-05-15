f858d0ad3038eb6cd06fed167dc06ed7aad84c74
Diana Catalina Solano Beltrán <dc.solano10@uniandes.edu.co>
2018-05-15 15:20:12 -0500

    lee qr

--- app/src/main/java/com/ingenian/icare/QRLectorActivity.java
+++ app/src/main/java/com/ingenian/icare/QRLectorActivity.java
@@ -2,6 +2,7 @@ package com.ingenian.icare;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,7 +19,7 @@ public class QRLectorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrlector);
-        dispatchTakePictureIntent();
+        leerQR();
     }
 
     private void dispatchTakePictureIntent() {
@@ -30,17 +31,34 @@ public class QRLectorActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                System.out.println("SE ESCANEÓ SÚPER HIPER DUPER BIEN: " + contents);
+            }
+            if(resultCode == RESULT_CANCELED){
+                //handle cancel
+                System.out.println("NO LO SÉ Y NUNCA LO SABRÉ");
+            }
         }
     }
 
     private void leerQR(){
-        FirebaseVisionBarcodeDetectorOptions options =
-                new FirebaseVisionBarcodeDetectorOptions.Builder()
-                        .setBarcodeFormats(
-                                FirebaseVisionBarcode.FORMAT_QR_CODE)
-                        .build();
+        try {
+
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+            startActivityForResult(intent, 0);
+
+        } catch (Exception e) {
+
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            startActivity(marketIntent);
+
+        }
     }
 }
